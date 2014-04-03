5c4a32838cfc1c88988373f37844264fbba079a9
fajoy <wuminfajoy@gmail.com>
2014-04-03 21:34:11 +0800

    add pelican note.

---
+++ content/rst/pelican.rst
@@ -0,0 +1,19 @@
+pelican 相關說明
+############################
+
+:category: 筆記
+:tags: pelican
+:slug: pelican
+
+文章連結
+=============
+
+- pelican入門導覽_ 
+
+其他文章
+==============
+
+- rst入門導覽_
+
+.. _pelican入門導覽: http://docs.getpelican.com/en/3.3.0/getting_started.html
+.. _rst入門導覽: http://docutils.sourceforge.net/docs/user/rst/quickref.html
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,10 +3,10 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'fajoy'
-SITENAME = u'Fajoy'
+SITENAME = u'Fajoy的筆記'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = u'zh-TW'
 
@@ -16,16 +16,24 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 
 DEFAULT_PAGINATION = False
 
 RELATIVE_URLS = True
+
+import os
+THEME = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                    'pelican-themes', 'pelican-bootstrap3')
+
+DEFAULT_DATE = "fs"
+DEFAULT_DATE_FORMAT ="%Y年%m月%d日"
+LOCALE = ("zh_TW.UTF8")
